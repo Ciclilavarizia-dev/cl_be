d1a978ffc11bbf4d8a88424575e1c40c3e8b12cd
Ericson Manalo <ericmana98@gmail.com>
2025-11-19 15:39:43 +0100

    Created a review mongoDB configuration

---
+++ Models/ReviewMDBConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace cl_be.Models
+{
+    public class ReviewMDBConfig
+    {
+        public string? ConnectionString { get; set; }
+
+        public string? DatabaseName { get; set; }
+
+        public string? ReviewsCollectionName { get; set; }
+
+    }
+}
