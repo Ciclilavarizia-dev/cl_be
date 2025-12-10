5446ce5a88f8fe9cadd81b61d5d425d89086fce3
Ericson Manalo <ericmana98@gmail.com>
2025-12-10 10:19:30 +0100

    Aggiungi aggiornamento profilo cliente
    È stato aggiunto un metodo privato `GetCustomerId()` per recuperare l'ID cliente dal JWT. Introdotto un nuovo endpoint `PUT api/customer/me` per aggiornare il profilo del cliente autenticato. Rimosso il metodo `GetCustomerProfileInfo(int customerId)` in favore di `GET api/customer/profile`. Aggiunto il file `CustomerUpdateDto.cs` per gestire i dati di aggiornamento del profilo.

---
+++ Models/Dto/CustomerDto/CustomerUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace cl_be.Models.Dto.CustomerDto
+{
+    public class CustomerUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Phone { get; set; }
+    }
+}
